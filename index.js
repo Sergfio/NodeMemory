9731bf2360a72af2fe949a5fc499f09c80ce4520
Stefan Focht <92666567+Sergfio@users.noreply.github.com>
2022-02-08 05:08:02 +0100

    first view

---
+++ index.js
@@ -0,0 +1,8 @@
+let os = require('os');
+let bytes = +os.freemem();
+let kbytes = bytes / 1000;
+let mbytes = kbytes / 1000;
+let gbytes = mbytes / 1000;
+
+let gbytesRounded = Math.round(gbytes);
+console.log('Total Memory' + ' ' + gbytesRounded + ' ' + 'gigabytes');
